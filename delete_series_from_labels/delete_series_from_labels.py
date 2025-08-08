cbf4a0bf9125b7c153090cc10e3c0ecae3cd27fe
Yannick Lemaréchal <10884038+ylemarechal@users.noreply.github.com>
2025-08-08 21:27:02 +0200

    ✨Delete Orthanc study from label

---
+++ delete_series_from_labels/delete_series_from_labels.py
@@ -0,0 +1,36 @@
+import os
+
+import dotenv
+import pyorthanc
+from pyorthanc import find_studies
+from urllib3.exceptions import HTTPError
+
+dotenv.load_dotenv('.env')
+
+orthanc_url = os.getenv('ORTHANC_URL')
+orthanc_username = os.getenv('ORTHANC_USERNAME')
+orthanc_password = os.getenv('ORTHANC_PASSWORD')
+
+client = pyorthanc.Orthanc(
+    orthanc_url,
+    username=orthanc_username,
+    password=orthanc_password,
+    timeout=240
+)
+
+studies = find_studies(
+    client=client,
+    labels=os.getenv('LABELS')
+)
+
+print(f'Found {len(studies)} studies with labels {os.getenv("LABELS")}')
+
+# Exit here to prevent unwanted deletion...
+exit(0)
+
+try:
+    for study in studies:
+        print(f'Deleting study {study.id_}...')
+        client.delete_studies_id(study.id_)
+except HTTPError:
+    print(f'Error in deleting study {study.id_}...')
